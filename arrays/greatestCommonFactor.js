164a70d45a6600302e732e450accd22fab54c399
Sho <sho.nozaki417@gmail.com>
2021-07-22 11:53:49 -0700

    brute force solution

--- arrays/greatestCommonFactor.js
+++ arrays/greatestCommonFactor.js
@@ -2,4 +2,35 @@
 
 // Write a function that returns the greatest common factor of an array of positive integers. 
 // Your return value should be a number, you will only receive positive integers.
-// greatestCommonFactor([46, 14, 20, 88]); // --> 2
+
+
+
+//Sort
+// Take smallest number and try to divide all other numbers by that number
+//then subtract and try again
+
+const isFactor = (arr, factor) => {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % factor !== 0) {
+			return false;
+		}
+	}
+	return true;
+};
+
+const greatestCommonFactor = (input) => {
+	let lowToHigh = input.sort();
+	let smallest = lowToHigh[0];
+	while (smallest > 0) {
+		if (isFactor(lowToHigh, smallest)) {
+			return smallest;
+		} else {
+			smallest -= 1;
+		}
+	}
+};
+
+
+console.log(greatestCommonFactor([46, 14, 20, 88])); // --> 2
+
+console.log(greatestCommonFactor([468, 156, 806, 312, 442])); // --> 26
