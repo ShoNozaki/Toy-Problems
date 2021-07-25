09f8bcef16fd9c681895d44d6add0840342652f0
Sho <sho.nozaki417@gmail.com>
2021-07-25 10:11:45 -0700

    added length checker and letter conversion table

--- algorithms/VINChecker.ts
+++ algorithms/VINChecker.ts
@@ -42,8 +42,37 @@
 // 5YJ3E1EAXHF000347 is a valid VIN.
 // Input Validation : VINs with lenghts different than 17 characters or containing invalid characters should return False as well.
 
-
+const letterTable = {
+	"A": 1,
+	"B": 2,
+	"C": 3,
+	"D": 4,
+	"E": 5,
+	"F": 6,
+	"G": 7,
+	"H": 8,
+	"J": 1,
+	"K": 2,
+	"L": 3,
+	"M": 4,
+	"N": 5,
+	"P": 7,
+	"R": 9,
+	"S": 2,
+	"T": 3,
+	"U": 4,
+	"V": 5,
+	"W": 6,
+	"X": 7,
+	"Y": 8,
+	"Z": 9
+}
 
 const isValidVin = (input: string): boolean => {
+	if(input.length !== 17){
+		return false
+	}
 
+
+	return true
 }
